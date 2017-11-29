b09688ecc45cfb05e8746f3c628752a198dfe620
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-11-29 11:39:13 -0200

    Fix likelihood ratio trick in jupyter notebook

--- utils.py
+++ utils.py
@@ -1,21 +1,13 @@
 import matplotlib.pyplot as plt
 
-def plot_results(parameter, losses, surrogate=None):
+def plot_results(*args):
     fig = plt.figure(figsize=(15, 3))
-
-    plt.subplot(131)
-    plt.plot(parameter, 'b-')
-    plt.title('parameter')
-    plt.ylabel('$\\theta$')
-    plt.grid()
-
-    plt.subplot(132)
-    plt.plot(losses, 'r-')
-    plt.title('losses')
-    plt.grid()
-
-    if surrogate is not None:
-        plt.subplot(133)
-        plt.plot(surrogate, 'g-')
-        plt.title('surrogate loss')
+    n = len(args)
+    for i, result in enumerate(args):
+        values, title, xlabel, ylabel = result
+        plt.subplot(1, n, i+1)
+        plt.plot(values, 'b-')
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
         plt.grid()
