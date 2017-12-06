effcd92f1d3578709a568a17a2f230d60c8faee9
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-12-06 11:12:59 -0200

    Refactor plots by adding utils.plot_function and utils.plot_hist

--- utils.py
+++ utils.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 
+
+def plot_function(ax, x, y, title, xlabel, ylabel):
+    ax.plot(x, y, 'b-')
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.grid()
+
+
+def plot_hist(ax, ys, title, xlabel):
+    for y, ylabel in ys:
+        ax.hist(y, bins=1000, histtype='step', normed=True, label=ylabel)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.grid()
+    ax.legend()
+
+
 def plot_results(*args):
     fig = plt.figure(figsize=(15, 3))
     n = len(args)
     for i, result in enumerate(args):
         values, title, xlabel, ylabel = result
-        plt.subplot(1, n, i+1)
-        plt.plot(values, 'b-')
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.grid()
+        ax = fig.add_subplot(1, n, i+1)
+        plot_function(ax, range(len(values)), values, title, xlabel, ylabel)
